a76750e68fcf765c576a6629a021dd57a29663b0
Silvano Hirtie <40663430+MyNameIsMental@users.noreply.github.com>
2019-08-21 17:09:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-	const Discord = require('discord.js');
+  const Discord = require('discord.js');
   const bot = new Discord.Client({ disableEveryone: true });
   const request = require('request');
   
